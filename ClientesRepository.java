adbb570c45abd60351bf5fa3a1e6da3ae51398dc
Jadersonsc <42000233+Jadersonsc@users.noreply.github.com>
2020-07-02 20:32:20 -0300

    Delete ClientesRepository.java

--- ClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.usj.ads.lpii.perfialaluminio;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientesRepository extends CrudRepository<Clientes, Long> {
-    List<Clientes> findAll();
-}
